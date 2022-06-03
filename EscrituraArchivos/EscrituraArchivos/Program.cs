59a62a9b8f630d8a2345cc4246b6b738232d4c19
AAngel777 <105814810+AAngel777@users.noreply.github.com>
2022-06-03 09:34:05 -0700

    EscrituraArchivos

---
+++ EscrituraArchivos/EscrituraArchivos/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace EscrituraArchivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter sw = new StreamWriter("ejemplo.txt", true);
+            // si el archivo no existe lo creara
+            // si el archivo existe, escribira en el
+            string[] lines =
+            {
+                "Esto es la primer linea",
+                "Esto es la segunda linea",
+                "Esto es la tercer linea"
+            };
+            // recorrer el arreglo para escriir las lineas en el archiv
+            foreach (string line in lines)
+            {
+                sw.WriteLine(line);
+            }
+            sw.Close();
+
+            Console.WriteLine("Escribiendo....");
+            Console.ReadLine();
+        } // cerrar el archivo
+
+    }
+}
+}
